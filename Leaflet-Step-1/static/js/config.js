aebedcf68ea0c2ebc2a3402036aa5d32920f8495
Thomas <tpnats@gmail.com>
2020-12-16 09:56:55 -0500

    10th commit, added images for readme

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-var API_KEY = "pk.eyJ1IjoibmF0b2xpMjkiLCJhIjoiY2tocnVmMG5nMDNrOTJxdDlyYzNuaXA5MiJ9.fEuM8ynar9tRQ2YSqy9GkA"
+var API_KEY = "pk.eyJ1IjoibmF0b2xpMjkiLCJhIjoiY2tpcmphbWN0MHAyYjMwbHJ5dW51ZXZpdyJ9.MDTpBniHgHOj_l33D3R2MA"
